003ef1d63c228f02fdb9001a7d8e712c5f9b30de
Oliver Schulz <oschulz@mpp.mpg.de>
2021-02-25 04:34:32 +0100

    Simplify SGE job status handling

--- templates/snakemake-config/cluster-sge/sge-submit.sh
+++ templates/snakemake-config/cluster-sge/sge-submit.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-jobstatusdir="${HOME}/batch-status"
-
 joblogdir="${HOME}/batch-logs"
 
 
@@ -11,33 +9,20 @@ scriptdir() {
 this_scriptdir=`scriptdir`
 
 
-# echo "Called submit-job.sh with args:" "$@" >> "$HOME/job-submission.log"
 
-runtime="1200"
-while getopts r: OPT; do
-	case ${OPT} in
-	r) runtime="${OPTARG}" ;;
-	esac
-done
-shift `expr $OPTIND - 1`
 jobscript="$1"
-
-# echo "Submitting $jobscript with runtime $runtime" >> "$HOME/job-submission.log"
 
 jobname=`basename "$jobscript" ".sh"`
 
 mkdir -p "$joblogdir"
-mkdir -p "$jobstatusdir"
 
 job_id=$(
     qsub \
-        -cwd \
-        -b y \
+        -cwd -V \
         -N "$jobname" \
         -o "$HOME/batch-logs/$jobname.stdout"  \
         -e "$HOME/batch-logs/$jobname.stderr" \
-        -l "h_rt=$runtime" \
-        "$this_scriptdir/job-wrapper.sh" "$jobscript" \
+        "$jobscript" \
     | head -n1 | sed 's/[^0-9]*\([0-9]\+\).*/\1/'
 )
 
